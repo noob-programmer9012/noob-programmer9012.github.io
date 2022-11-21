73d8f69b669a5e0069075d529965669be49678f1
Parth Patel <parth.sclub@gmail.com>
2022-11-22 00:53:49 +0530

    overhaul

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-let save = document.querySelector('#save')
-let trans = document.querySelector('#trans')
-let gst = document.querySelector('#gst')
-let allowed = /^[A-Z-_&]*$/i
-let error = document.querySelector('.error')
-let cancel = document.querySelector('#cancel')
+const toggleNav = document.querySelector('#mobile')
+const sidebar = document.querySelector('.sidebar')
+const main = document.querySelector('.main')
+const modal = document.querySelector('#modal')
+const confirm = document.querySelector('#confirm')
+const closeModal = document.querySelectorAll('#modal-close')
 
-function filerTable () {
-  var input, filter, table, tr, td, i, txtValue
+closeModal.forEach(item => {
+  item.addEventListener('click', () => {
+    modal.classList.remove('show-modal')
+  })
+})
+
+function filterTable () {
+  let input, filter, table, tr, td, i, txtValue
   input = document.getElementById('search')
   filter = input.value.toUpperCase()
   table = document.getElementById('myTable')
@@ -26,28 +32,15 @@ function filerTable () {
   }
 }
 
-trans.addEventListener('keydown', e => {
-  if (e.key.match(allowed)) {
-    error.style.display = 'none'
-    return
+function clicked () {
+  sidebar.classList.toggle('show')
+  if (sidebar.style.width === '270px') {
+    sidebar.style.width = '60px'
   } else {
-    e.preventDefault()
-    error.style.display = 'block'
-    error.innerHTML = 'No numbers allowed'
+    sidebar.style.width = '270px'
   }
-})
-
-gst.addEventListener('keydown', () => {
-  if (error.style.display === 'block') {
-    error.style.display = 'none'
-  }
-})
-
-let tbody = document.querySelector('tbody')
-let confirm = document.querySelector('#confirm')
-let tr, td0, td1, td2, td3, key, value, edit, remove
-let form = document.querySelector('form')
-let modalbody = document.querySelector('#modalbody')
+}
+toggleNav.addEventListener('click', clicked)
 
 function setAttributes (elem, attr) {
   for (let key in attr) {
@@ -55,13 +48,80 @@ function setAttributes (elem, attr) {
   }
 }
 
+let obj = new Object()
+for (let i = 0; i < localStorage.length; i++) {
+  obj[localStorage.key(i)] = localStorage.getItem(localStorage.key(i))
+}
+
+let tbody = document.querySelector('tbody')
+const save = document.querySelector('#save')
+
+function duplicate (transporter, gst) {
+  for (const elem in obj) {
+    if (
+      `${elem.toUpperCase().trim()}` === transporter.toUpperCase() &&
+      `${obj[elem].toUpperCase().trim()}` === gst.toUpperCase()
+    ) {
+      return true
+    }
+  }
+  return false
+}
+
+save.addEventListener('click', saveData)
+function saveData (e) {
+  e.preventDefault()
+  if (trans.value !== '' || gst.value !== '') {
+    if (e.target.innerText === 'Submit') {
+      if (!duplicate(trans.value, gst.value)) {
+        localStorage.setItem(
+          trans.value.toUpperCase().trim(),
+          gst.value.toUpperCase().trim()
+        )
+        window.location.reload()
+      } else {
+        alert('Duplicate Entry')
+      }
+    } else if (e.target.innerText === 'Change') {
+      if (!duplicate(trans.value, gst.value)) {
+        localStorage.removeItem(e.target.value)
+        localStorage.setItem(
+          trans.value.toUpperCase().trim(),
+          gst.value.toUpperCase().trim()
+        )
+        e.target.innerText = 'Submit'
+        save.click()
+      } else {
+        alert('Duplicate Entry')
+      }
+    }
+  } else {
+    alert('empty')
+  }
+}
+
+let tr, td0, td1, td2, td3, key, value, edit, remove
+
+function removeData (e) {
+  localStorage.removeItem(e.target.value)
+  window.location.reload()
+}
+confirm.addEventListener('click', removeData)
+
+modal.addEventListener('click', e => {
+  if (e.target.id === 'modal') {
+    modal.classList.remove('show-modal')
+  }
+})
+
 document.addEventListener('DOMContentLoaded', () => {
   // Load Data from local storage
-  for (let i = 0; i < localStorage.length; i++) {
+  const data = Object.keys(obj).sort()
+  for (let i = 0; i < data.length; i++) {
     // Dynamically add data to the table
     counter = document.createTextNode(i + 1)
-    key = document.createTextNode(localStorage.key(i))
-    value = document.createTextNode(localStorage.getItem(localStorage.key(i)))
+    key = document.createTextNode(data[i])
+    value = document.createTextNode(obj[data[i]])
 
     // Print Values on table
     tr = document.createElement('tr')
@@ -77,7 +137,6 @@ document.addEventListener('DOMContentLoaded', () => {
     tr.appendChild(td1)
     tr.appendChild(td2)
 
-    // Add control to the table
     edit = document.createElement('span')
     edit.innerText = 'edit'
     setAttributes(edit, {
@@ -100,93 +159,29 @@ document.addEventListener('DOMContentLoaded', () => {
     tr.appendChild(td3)
     tbody.appendChild(tr)
   }
-  // Give modal delete button value
+
+  // Remove elements
   remove = document.querySelectorAll('#remove')
-  remove.forEach(element => {
-    element.addEventListener('click', e => {
-      confirm.setAttribute('value', `${e.target.title}`)
-      modalbody.innerHTML = `Are you sure you want to delete ${localStorage.key(
-        e.target.title
-      )}`
+  remove.forEach(item => {
+    item.addEventListener('click', e => {
+      const modalBody = document.querySelector('.modal-body')
+      modalBody.innerHTML = `Delete ${data[e.target.title]}?`
+      modal.classList.add('show-modal')
+      const confirm = document.querySelector('#confirm')
+      confirm.value = data[e.target.title]
     })
   })
 
-  // Edit Data
+  // Edit elements
   edit = document.querySelectorAll('#edit')
-  edit.forEach(element => {
-    element.addEventListener('click', e => {
+  edit.forEach(item => {
+    item.addEventListener('click', e => {
       save.innerText = 'Change'
-      save.setAttribute('value', e.target.title)
-      trans.value = localStorage.key(e.target.title)
-      gst.value = localStorage.getItem(localStorage.key(e.target.title))
-      cancel.style.display = 'block'
+      trans.value = data[e.target.title]
+      gst.value = obj[trans.value]
+      save.value = data[e.target.title]
+      const cancel = document.querySelector('#cancel')
+      cancel.classList.add('show')
     })
   })
-  // End of DOM
-})
-
-// Check if transporter already in Local storage
-function duplicate (transporter, gst) {
-  for (let i = 0; i < localStorage.length; i++) {
-    if (
-      localStorage.key(i) === transporter &&
-      localStorage.getItem(localStorage.key(i)) === gst
-    ) {
-      return true
-    }
-  }
-  return false
-}
-// Save data to local Storage
-save.addEventListener('click', e => {
-  e.preventDefault()
-  if (save.innerText === 'Submit') {
-    if (
-      trans.value !== '' &&
-      gst.value !== '' &&
-      !duplicate(trans.value, gst.value)
-    ) {
-      localStorage.setItem(`${trans.value}`, `${gst.value}`)
-      form.reset()
-      window.location.reload()
-    } else {
-      if (trans.value === '' || gst.value === '') {
-        error.style.display = 'block'
-        error.innerHTML = 'Input values must not be blank.'
-      } else {
-        error.style.display = 'block'
-        error.innerHTML = 'Duplicate entry found.'
-      }
-    }
-  }
-  if (save.innerText === 'Change') {
-    if (
-      trans.value !== '' &&
-      gst.value !== '' &&
-      !duplicate(trans.value, gst.value)
-    ) {
-      localStorage.removeItem(localStorage.key(e.target.value))
-      localStorage.setItem(trans.value, gst.value)
-      form.reset()
-      window.location.reload()
-    } else {
-      if (trans.value === '' || gst.value === '') {
-        error.style.display = 'block'
-        error.innerHTML = 'Input values must not be blank.'
-      } else {
-        error.style.display = 'block'
-        error.innerHTML = 'Duplicate entry found.'
-      }
-    }
-  }
-})
-
-// Delete Data
-confirm.addEventListener('click', e => {
-  localStorage.removeItem(localStorage.key(e.target.value))
-  window.location.reload()
-})
-
-cancel.addEventListener('click', e => {
-  window.location.reload()
 })
